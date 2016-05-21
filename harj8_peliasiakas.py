be31f4b114c657a569fbb52c939783c073395714
balduri <bldr86@gmail.com>
2016-05-21 09:29:07 +0300

    Initial commit

---
+++ harj8_peliasiakas.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Peliasiakas
+# Ottaa yhteyden UDP:llä pelipalvelimeen
+# Pelin tarkoituksena on arvata oikea numero
+# moninpelissä, missä palvelin arpoo numeron
+# ja ilmoittaa voittajan
+
+
+# TODO: Peruskommunikaatio
+# TODO: JOIN -tila
+# TODO: GAME -tila
+
+import socket
+import sys, select
+if sys.platform == 'win32':
+    import msvcrt
+
+HOST = '127.0.0.1'
+PORT = 24001
+size = 1024
+name = ''
+
+# Read a line. Using select for non blocking reading of sys.stdin
+def getLine():
+    i,o,e = select.select([sys.stdin],[],[],0.0001)
+
+    for s in i:
+        if s == sys.stdin:
+            input = sys.stdin.readline()
+            return input
+    return False
+
+def join():
+    name = raw_input('Name:')
+    return
+
+def game():
+    pass
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
+# Jos käyttöjärjestelmänä Linux tai OSX
+if sys.platform == 'linux' or sys.platform == 'linux2' or sys.platform == 'darwin':
+    while True:
+
+        try:
+            s.settimeout(2)
+            recv_data, addr = s.recvfrom(size)
+            if recv_data:
+                sys.stdout.write(recv_data[0:recv_data.find(';')] + ': ' +
+                                 recv_data[recv_data.find(';') + 1:])
+
+        except socket.timeout:
+            pass
+
+        input = getLine()
+        if input != False:
+                s.sendto(name + ';' + input, (HOST, PORT))
+
+# Jos käyttöjärjestelmänä windows
+else:
+    while True:
+        ready = select.select([s], [], [], 0.001)
+        if ready[0]:
+            recv_data = s.recv(size)
+            sys.stdout.write(recv_data[0:recv_data.find(';')] + ': ' +
+                             recv_data[recv_data.find(';') + 1:] + '\n')
+        else:
+            if msvcrt.kbhit():
+                char = msvcrt.getch()
+                if char == "\r":
+                    s.sendto(name + ';' + raw_input(">"), (HOST, PORT))
