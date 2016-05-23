5b37b1272b800a7bac256fefc519d616c7557d20
Antti Ollila <antti.ollila@balduri.fi>
2016-05-23 21:54:44 +0300

    Yksinkertainen virheenkäsittely

--- harj8_peliasiakas.py
+++ harj8_peliasiakas.py
@@ -32,26 +32,34 @@ def checkmessage(message, server):
     print code
     global datagiven
     global gameover
+
     if status == 'QUIT':
     # ack('500', server)
         s.sendto('ACK;500'.rstrip(), server)
         gameover = True
         sys.exit(1)
+
     elif status == 'ACK':
         if code == '300':
             ack(code, server)
+
         elif status == 'ACK' and code == '202':
             data(server)
+
         elif status == '203':
             return
+
     elif status == 'DATA':
         if datagiven:
             ack('300', server)
+
             if not gameover:
                 data(server)
+
         else:
             data(server)
         return
+
     else:
         if code[0:1] == '4':
             print 'Error: ' + code + status
